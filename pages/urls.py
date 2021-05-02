91f8c00276e0d65dfde3b5326f916999c457e279
Mateberson <mateus.pereira.r@gmail.com>
2021-05-02 16:12:53 -0300

    Revert "Pagina home com uma ficha carregada e pagina da ficha carregando informações"
    This reverts commit f1a96362

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import sheet, home
+from .views import home
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('ficha/', sheet, name='ficha'),
     path("password_reset", views.password_reset_request, name="password_reset"),
 ]
