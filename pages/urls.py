4c5d2de2de748b2e019eaa3360a4e0e3e12f4aac
Mateberson <mateus.pereira.r@gmail.com>
2021-05-02 16:40:21 -0300

    Ficha carregando informações

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import home
+from .views import ficha, home
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('ficha/', ficha, name='ficha'),
     path("password_reset", views.password_reset_request, name="password_reset"),
 ]
